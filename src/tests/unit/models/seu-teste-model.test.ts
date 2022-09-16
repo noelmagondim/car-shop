d57989cde703500fd4c48a80435323a3bc78f014
noelmagondim <noelmagondim@gmail.com>
2022-09-16 15:21:17 -0300

    Desafio 05 e 10 concluído

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,22 +1,43 @@
-// template para criação dos testes de cobertura da camada de model
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { Model } from 'mongoose';
+import { ErrorTypes } from '../../../errors/catalog';
+import CarModel from '../../../models/CarModel';
+import { ICar } from '../../../interfaces/ICar';
 
+const carTest:ICar = {
+  model: "Fiat Uno",
+  year: 2012,
+  color: "white",
+  buyValue: 31500,
+  doorsQty: 2,
+  seatsQty: 5,
+};
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+const carWithIdTest:ICar & { _id:string } = {
+  _id: "5fee51c97873f1gc23111114",
+  model: "Fiat Uno",
+  year: 2012,
+  color: "white",
+  buyValue: 31500,
+  doorsQty: 2,
+  seatsQty: 5,
+};
 
-// describe('Sua descrição', () => {
+describe('Camada CarModel.ts', () => {
+  const carModel = new CarModel();
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  before(async () => {
+    sinon.stub(Model, 'create').resolves(carWithIdTest);
+  });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  after(()=>{
+    sinon.restore();
+  })
 
-//   it('', async () => {});
-
-// });
+  it('Criando carro', async () => {
+    const result = await carModel.create(carTest);
+    expect(result).to.be.deep.equal(carWithIdTest);
+  });
+});
