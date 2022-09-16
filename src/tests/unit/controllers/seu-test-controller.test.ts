cba1243caa000244476a4f30ea18a13e981e3fdb
noelmagondim <noelmagondim@gmail.com>
2022-09-16 15:54:08 -0300

    Desafio 07 e 12: concluídos

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -1,22 +1,42 @@
-// template para criação dos testes de cobertura da camada de controller
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../services/CarService';
+import { ICar  } from '../../../interfaces/ICar';
+import CarController from '../../../controllers/CarController';
+import { Request, Response } from 'express';
 
+const carTest:ICar = {
+  model: "Fiat Uno",
+  year: 2012,
+  color: "white",
+  buyValue: 31500,
+  doorsQty: 2,
+  seatsQty: 5,
+};
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+describe('Camada CarController.ts', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+  const req = {} as Request;
+  const res = {} as Response;
 
-// describe('Sua descrição', () => {
+  before(async () => {
+    sinon.stub(carService, 'create').resolves(carTest);
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
-
-//   after(()=>{
-//     sinon.restore();
-//   })
-
-//   it('', async () => {});
-
-// });
+  after(()=>{
+    sinon.restore();
+  })
+  
+  it('Criando carro', async () => {
+    req.body = carTest;
+    await carController.create(req, res);
+    expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+    expect((res.json as sinon.SinonStub).calledWith(carTest)).to.be.true;
+  });
+});
